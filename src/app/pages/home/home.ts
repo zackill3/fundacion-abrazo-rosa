eabcceeb4094dda4643629e60ce1383b926769f6
zackill3 <julian.gonzalez.cocicor@gmail.com>
2026-07-22 23:13:03 -0500

    Delete src directory

--- src/app/pages/home/home.ts
+++
@@ -1,5 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
-@Component({ selector: 'app-home', imports: [RouterLink], templateUrl: './home.html', styleUrl: './home.scss' })
-export class Home {}
