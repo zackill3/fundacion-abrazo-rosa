6628236ff73d94639faf08fe776c9fe740774c82
zackill3 <julian.gonzalez.cocicor@gmail.com>
2026-07-22 23:17:33 -0500

    nuevamente

---
+++ src/app/pages/presence/presence.ts
@@ -0,0 +1,3 @@
+import { Component } from '@angular/core';
+@Component({ selector: 'app-presence', templateUrl: './presence.html', styleUrl: './presence.scss' })
+export class Presence {}
---
+++ src/app/pages/programs/programs.ts
@@ -0,0 +1,3 @@
+import { Component } from '@angular/core';
+@Component({ selector: 'app-programs', templateUrl: './programs.html', styleUrl: './programs.scss' })
+export class Programs {}
