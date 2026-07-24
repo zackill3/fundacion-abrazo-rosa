4caa95e462ec5cb5ae2e1f3cd426904347e62c35
zackill3 <julian.gonzalez.cocicor@gmail.com>
2026-07-24 02:15:14 -0500

    Delete server directory

--- server/types.ts
+++
@@ -1,4 +0,0 @@
-import type { Request } from 'express';
-
-export interface AuthUser { id: number; name: string; email: string; role: 'admin' | 'user'; }
-export interface AuthenticatedRequest extends Request { user?: AuthUser; }
